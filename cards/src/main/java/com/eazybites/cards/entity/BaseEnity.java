3cc0758f880f949b04294cd5fb654c9e911523c2
IrFaN AaDeEzZ <mailmeirfan1993@gmail.com>
2025-01-09 11:37:56 +0000

    Base entity added in acrds

---
+++ cards/src/main/java/com/eazybites/cards/entity/BaseEnity.java
@@ -0,0 +1,33 @@
+package com.eazybites.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDate;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDate createdAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDate updatedAt;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private String updatedBy;
+
+}
